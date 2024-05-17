1c2d7a0b84ad7c1f9db1d4398e0a2f9085f226f7
Ayush Rastogi <ayushrastogi689@gmail.com>
2024-05-17 17:45:11 +0530

    Updated MainBody component.

--- google_form/src/components/Body/MainBody.js
+++ google_form/src/components/Body/MainBody.js
@@ -5,6 +5,7 @@ import StorageIcon from "@mui/icons-material/Storage";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import FolderOpenIcon from "@mui/icons-material/FolderOpen";
 import { IconButton } from "@mui/material/";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 const MainBody = () => {
   return (
@@ -23,7 +24,29 @@ const MainBody = () => {
             <FolderOpenIcon style={{ fontSize: "1rem", color: "black" }} />
           </IconButton>
         </div>
-        <div className="mainbody_docs"></div>
+        <div className="mainbody_docs">
+          <div className="docs_card">
+            <img src="" alt="" />
+            <div className="doc_card_content">
+              <h5></h5>
+              <div className="doc_content">
+                <div className="content_left">
+                  <StorageIcon
+                    style={{
+                      fontSize: "0.75rem",
+                      color: "white",
+                      backgroundColor: "#6e2594",
+                      padding: "0.4rem",
+                      marginRight: "0.25rem",
+                      borderRadius: "2px",
+                    }}
+                  />
+                </div>
+                <MoreVertIcon />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
