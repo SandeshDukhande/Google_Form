9cf14a187a6af619745302ed4caf58c83e453119
Ayush Rastogi <ayushrastogi689@gmail.com>
2024-05-17 16:30:39 +0530

    Added Header, Template and Main Body component to Home page.

--- google_form/src/pages/Home/Home.js
+++ google_form/src/pages/Home/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "../../components/Header/Header";
+import Template from "./../../components/Template/Template";
+import MainBody from "../../components/Body/MainBody";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Template />
+      <MainBody />
+    </div>
+  );
 };
 
 export default Home;
