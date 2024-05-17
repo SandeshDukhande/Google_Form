bc6d9d248cf9e63123032b5f25a79215ab97da1c
Ayush Rastogi <ayushrastogi689@gmail.com>
2024-05-17 16:27:59 +0530

    Added Mian Body component to our form

--- google_form/src/components/Body/MainBody.js
+++ google_form/src/components/Body/MainBody.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import "./MainBody.css";
+
+import StorageIcon from "@mui/icons-material/Storage";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import FolderOpenIcon from "@mui/icons-material/FolderOpen";
+import { IconButton } from "@mui/material/";
 
 const MainBody = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="mainbody">
+      <div className="mainbody_top">
+        <div className="mainbody_top_left">Recent Forms</div>
 
-export default MainBody
+        <div className="mainbody_top_right">
+          <div className="mainbody_top_center">
+            Owned By Anyone <ArrowDropDownIcon />
+          </div>
+          <IconButton>
+            <StorageIcon style={{ fontSize: "1rem", color: "black" }} />
+          </IconButton>
+          <IconButton>
+            <FolderOpenIcon style={{ fontSize: "1rem", color: "black" }} />
+          </IconButton>
+        </div>
+        <div className="mainbody_docs"></div>
+      </div>
+    </div>
+  );
+};
+
+export default MainBody;
