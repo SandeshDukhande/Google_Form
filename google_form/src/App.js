9678241301c47065a78be16d6175019e9cd0e7d5
Ayush Rastogi <ayushrastogi689@gmail.com>
2024-05-19 14:07:05 +0530

    Added Routing for Create New Form page.

--- google_form/src/App.js
+++ google_form/src/App.js
@@ -1,66 +1,7 @@
 import "./App.css";
-import FormHeader from "./components/FormHeader/FormHeader";
-import Header from "./components/Header/Header";
-import Template from "./components/Template/Template";
+import CreateNewForm from "./components/CreateForm/CreateNewForm";
 import Home from "./pages/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import React, { useState } from "react";
-
-// function App() {
-//   const [name, setName] = useState("");
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-
-//   async function resgisterUser(event) {
-//     event.preventDefault();
-//     const response = await fetch("https://localhost:3500/api/register", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         name,
-//         email,
-//         password,
-//       }),
-//     });
-
-//     const data = await response.json();
-//     console.log(data);
-//   }
-
-//   return (
-//     <div>
-//       <h1>Registration Page</h1>
-//       <form action="" id="registration-form">
-//         <input
-//           type="text"
-//           placeholder="Full Name"
-//           value={name}
-//           onChange={(e) => setName(e.target.value)}
-//         />
-//         <br />
-//         <input
-//           type="email"
-//           placeholder="Email Id"
-//           value={email}
-//           onChange={(e) => setEmail(e.target.value)}
-//         />
-//         <br />
-//         <input
-//           type="password"
-//           placeholder="Password"
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//         />
-//         <br />
-//         <input type="submit" />
-//       </form>
-//     </div>
-//   );
-// }
-
-// export default App;
 
 function App() {
   return (
@@ -68,7 +9,7 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/form" element={<FormHeader />} />
+          <Route path="/form/:id" element={<CreateNewForm />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
