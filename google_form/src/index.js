7823781dad8c8a32c86535542a2a586cf7485e73
Ayush Rastogi <ayushrastogi689@gmail.com>
2024-05-20 21:49:42 +0530

    Provided redux store to app.

--- google_form/src/index.js
+++ google_form/src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { StateContext, StateProvider } from "./components/redux/StateProvider";
+import reducer, { initialState } from "./components/redux/reducer/reducer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>
 );
