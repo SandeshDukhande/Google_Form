9cb4b1a985a46d7e1dc27388b8e08ffb57b04581
Ayush Rastogi <ayushrastogi689@gmail.com>
2024-05-20 21:16:04 +0530

    Added Routing for Contact Us page

--- google_form/src/App.js
+++ google_form/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./pages/Signup/Signup";
 import Login from "./pages/Login/Login";
 import NotFound from "./pages/Not Found/NotFound";
+import Contact from "./pages/Contact/Contact";
 
 function App() {
   const user = localStorage.getItem("token");
@@ -17,6 +18,7 @@ function App() {
           <Route path="/login" exact element={<Login />} />
           <Route path="/" element={<Navigate replace to="/Login" />} />
           <Route path="/form/:id" element={<CreateNewForm />}></Route>
+          <Route path="/contact" element={<Contact />} />
           <Route path="/*" element={<NotFound />} />
         </Routes>
       </div>
