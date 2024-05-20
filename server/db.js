be1c0b6d184036a23b8bc20f85b3315a8386bd44
Ayush Rastogi <ayushrastogi689@gmail.com>
2024-05-20 22:27:57 +0530

    Updated db connection

--- server/db.js
+++ server/db.js
@@ -1,15 +1,17 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-module.exports = () => {
-	const connectionParams = {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	};
-	try {
-		mongoose.connect(process.env.DB, connectionParams);
-		console.log("Connected to database successfully");
-	} catch (error) {
-		console.log(error);
-		console.log("Could not connect database!");
-	}
+const connectDB = async () => {
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  try {
+    await mongoose.connect(process.env.DB, connectionParams);
+    console.log("Connected to database successfully");
+  } catch (error) {
+    console.error("Could not connect to the database!", error);
+  }
 };
+
+export default connectDB;
